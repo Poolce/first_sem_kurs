03f17b525ed6bf3db2f32693574785049796defa
IlinR <62259364+IlinR@users.noreply.github.com>
2022-11-11 19:35:41 +0300

    H

--- main.py
+++ main.py
@@ -7,7 +7,11 @@ row_table = 30 #Количество строк в таблицах
 gui.get_table('Macroparams',['M1','M2','M3','M4','M5','M6','M7','M8'],[[i] for i in range(row_table)],np.transpose(gs.Macroparameters)[:row_table])
 gui.get_table('Norm macroparams',['M1','M2','M3','M4','M5','M6','M7','M8'],[[i] for i in range(row_table)],np.transpose(gs.norm_Macroparameters)[:row_table])
 
-for i in range(len(gs.norm_Macroparameters)):
-    gui.get_gistogram(gs.norm_Macroparameters[i],"M"+str(i+1))
+# for i in range(len(gs.norm_Macroparameters)):
+#     gui.get_gistogram(gs.norm_Macroparameters[i],"M"+str(i+1))
+
+gui.get_gistograms(gs.norm_Macroparameters,['M'+str(i+1)for i in range(len(gs.norm_Macroparameters))],4)
 
 gui.get_correllation(gs.norm_Macroparameters,["M1","M2","M3","M4","M5","M6","M7","M8"])
+
+gui.get_scatter(gs.norm_Macroparameters[1],)
--- source/graphical_interface.py
+++ source/graphical_interface.py
@@ -33,7 +33,20 @@ def get_gistogram(array, tittle):
     histMp.set_title(tittle)
     plt.show()
 
+def get_gistograms(array, tittles,col = 1):
+    
+    rows = int(len(array)/col) + (len(array) % col>0)
+        
+    fig, histMp = plt.subplots(rows,col)
 
+    for i in range(rows):
+        for j in range(col):
+            histMp[i][j].hist(array[i*col+j], bins=50, linewidth=0.5, edgecolor="white")
+            a_min = min(array[i*col+j])
+            a_max = max(array[i*col+j])
+            histMp[i][j].set(xlim=(a_min, a_max), xticks=np.linspace(a_min, a_max, 9))
+            histMp[i][j].set_title(tittles[i*col+j])
+    plt.show()
 
 
 def get_correllation(array, arg_names):
@@ -41,7 +54,7 @@ def get_correllation(array, arg_names):
     
     fig, cor = plt.subplots()
     im = cor.imshow(array_cor)
-
+    cbar = fig.colorbar(im)
     cor.set_xticks(np.arange(8), labels=arg_names)
     cor.set_yticks(np.arange(8), labels=arg_names)
 
@@ -52,3 +65,14 @@ def get_correllation(array, arg_names):
 
     fig.tight_layout()
     plt.show()
+
+def get_scatter(array_1,array_2):
+    fig, scat = plt.subplots()
+    # cor=np.corrcoef()
+    colors = np.random.uniform(15, 80, len(array_1))
+    scat.scatter(array_1,array_2,s=10,c='blue')
+    scat.scatter(array_2,array_1,s=10,c='red')
+    scat.set(xlim=(0, 8), xticks=np.arange(1, 8),
+             ylim=(0, 8), yticks=np.arange(1, 8))
+    
+    plt.show
